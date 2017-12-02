50b236f9013fbc802fd998a5410723f96c730d94
Wayne Cheng <wayne@che.ng>
2017-12-01 21:18:04 -0800

    peripherals put into modal

--- src/OnePage.js
+++ src/OnePage.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import API from "./utils/API";
-import ContentEditable from 'react-contenteditable';
-import UserForm from './components/UserForm';
+import ContentEditable from "react-contenteditable";
+import UserForm from "./components/UserForm";
 
 const breakpoint = 24;
 
@@ -13,8 +13,7 @@ class OnePage extends Component {
 		super(props);
 		this.state = {
 			content: "Just some text.",
-			split: [],
-
+			split: []
 		};
 	}
 
@@ -22,89 +21,88 @@ class OnePage extends Component {
 	componentDidMount = () => {
 		console.log("---- componentDidMount --->");
 
-		let {id} = this.props;
-		console.log('id',id);
+		let { id } = this.props;
+		// console.log("> id", id);
 
 		API.getOne(id).then(response => {
-			console.log("response", response);
-			console.log("response.data", response.data);
-			let { text } = response.data;
+			// console.log("> response.data", response.data);
+			let { id,text } = response.data;
+			console.log('> id',id);
+			console.log('> text',text);
 
 			this.setState({
 				id: id,
 				initial: text,
-				content: text,
+				content: text
 			});
 		});
+	};
 
-		// <article id="doc" className="content-wrap" contentEditable="true" onKeyUp={this.handleChange} >
+	//==================================================
+	handleMenuOpen = event => {
+		const modalRootEl = document.getElementById("modal-root");
+		modalRootEl.classList.remove("closed");
 
-
-		// this.setState({
-		// 	content
-		// });
+		// const mainEl = document.querySelector('main');
+		const mainEl = document.querySelector('#doc');
+		mainEl.classList.add('blurred-out');
 	};
 
 	//==================================================
 	handleChange = event => {
 		this.setState({
 			content: event.target.value
-		})
+		});
 	};
 	//==================================================
 	handleSave = event => {
 		event.preventDefault();
 		let { id, content } = this.state;
 
-		console.log('id',id);
-		console.log('content',content);		
+		console.log("id", id);
+		console.log("content", content);
 
-		
-		API.update(id,content)
-		
-	}
+		API.update(id, content);
+	};
 	//==================================================
 
 	render() {
 		return (
 			<div id="justoneroot">
-				<a className="sitename-wrap" href="https://github.com/wayncheng/justonepage" >
+				<a
+					className="sitename-wrap"
+					href="https://github.com/wayncheng/justonepage"
+				>
 					<h1 className="sitename"> justonepage </h1>
 				</a>
 				<nav className="globalnav">
-				<a className="gn-burger" href="#!">
-					<img src="icons/burger-black-a-round.svg"/>
-				</a>
-					<button 
-						id="save-btn" 
-						className="ws-btn ws-light ws-mini"
-						onClick={this.handleSave}
+					<a
+						className="gn-burger"
+						href="#!"
+						onClick={this.handleMenuOpen}
 					>
-						Save
-					</button>
+						<img src="icons/burger-black-a-round.svg" />
+					</a>
 				</nav>
 				<main>
-					{/* <article
-						id="doc" 
-						className="content-wrap"
-						html={this.state.content}
-						disabled={false} 
-						onChange={this.handleChange}
-						placeholder="Just some placeholder."
-					>
-						sartorial viral bicycle rights XOXO ugh keytar flexitarian heirloom Pinterest Truffaut Thundercats kale chips +1 banjo Carles selvage quinoa Godard YOLO normcore Marfa tattooed hashtag letterpress mustache occupy post-ironic cliche Williamsburg artisan bespoke Schlitz hoodie lomo church-key mlkshk vinyl small batch Intelligentsia Pitchfork Vice butcher shabby chic pug DIY chia farm-to-table plaid disrupt dreamcatcher pour-over cray Portland you probably haven't heard of them tote bag sustainable Banksy irony cornhole bitters American Apparel cardigan PBR synth 3 wolf moon asymmetrical next level hella squid semiotics ennui master cleanse Echo Park leggings VHS Tonx meh drinking vinegar readymade sriracha craft beer fanny pack salvia gastropub single-origin coffee Blue Bottle authentic Tumblr actually mixtape Etsy Wes Anderson crucifix wolf locavore stumptown chillwave ethical Neutra banh mi trust fund kogi mumblecore kitsch art party beard raw denim polaroid before they sold out selfies pop-up keffiyeh Odd Future aesthetic yr McSweeney's brunch roof party distillery chambray lo-fi photo booth PBR&B Cosby sweater direct trade  scenester skateboard Bushwick cred fap Brooklyn fingerstache narwhal paleo forage tofu Shoreditch food truck pork belly umami typewriter pickled whatever try-hard put a bird on it wayfarers four loko street art deep v Helvetica fashion axe gluten-free literally High Life flannel blog biodiesel Kickstarter 8-bit slow-carb organic freegan twee fixie seitan 90's messenger bag meggings iPhone Austin gentrify swag vegan jean shorts retro tousled
-					</article> */}
 					<ContentEditable
-						id="doc" 
+						id="doc"
 						className="content-wrap"
 						html={this.state.content}
-						disabled={false} 
+						disabled={false}
 						onChange={this.handleChange}
 						placeholder="Just some placeholder."
 					/>
 				</main>
-				{/* <UserForm /> */}
-				
+				<UserForm>
+					<button
+						id="save-btn"
+						className="ws-btn ws-light pin-top-left"
+						onClick={this.handleSave}
+					>
+						Save
+					</button>
+				</UserForm>
 			</div>
 		);
 	}
@@ -112,5 +110,5 @@ class OnePage extends Component {
 export default OnePage;
 
 OnePage.defaultProps = {
-	id: '1',
-}
+	id: "1"
+};
--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,66 +1,132 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 import API from "../utils/API";
 
 class UserForm extends Component {
-	constructor(props){
+	constructor(props) {
 		super(props);
 		this.state = {
-			
-		}
+			authType: "Login",
+			uuid: "",
+			password: "",
+			newUser: false
+		};
 	}
+	//==================================================
+	handleChange = event => {
+		const { name, value } = event.target;
 
-	handleFormSubmit = (event) => {
+		this.setState({
+			[name]: value
+		});
+	};
+	//==================================================
+	handleAuthType = event => {
+		this.setState({ newUser: event.target.checked });
+	};
+	//==================================================
+
+	handleFormSubmit = event => {
 		event.preventDefault();
-		const username = document.getElementById('username').value;
-		const password = document.getElementById('password').value;
-		const newUser = document.getElementById('new-user-flag').checked;
+		// const username = document.getElementById("username").value;
+		// const password = document.getElementById("password").value;
+		// const newUser = document.getElementById("new-user-flag").checked;
 
-		console.log('username',username);
-		console.log('password',password);
+		const { uuid, password, newUser } = this.state;
 
-		if (newUser){
-			console.log('---- SIGNUP --->');
+		if (newUser) {
+			console.log("---- CREATE --->");
 			////////////////////////////////////////////////////
 			// Sign Up Process Starts Here
 			////////////////////////////////////////////////////
-		}
-		else {
-			console.log('---- LOGIN --->');
+		} else {
+			console.log("---- LOGIN --->");
 			////////////////////////////////////////////////////
 			// Login Process Starts Here
 			////////////////////////////////////////////////////
-
 		}
-	}
 
-	render(){
+		console.log("> uuid:", uuid);
+		console.log("> password:", password);
+		console.log("> newUser:", newUser);
+	};
+
+	handleFormClose = event => {
+		event.preventDefault();
+		// this.closeModal();
+
+		const { target } = event;
+		const validTargets = ["modal-root", "close-form"];
+
+		if (validTargets.indexOf(target.id) !== -1) {
+			this.closeModal();
+		} else {
+			console.log("not closing modal");
+		}
+	};
+
+	closeModal = () => {
+		const formRoot = document.getElementById("modal-root");
+		formRoot.classList.add("closed");
+		
+		// const mainEl = document.querySelector('main');
+		const mainEl = document.querySelector('#doc');
+		mainEl.classList.remove('blurred-out');
+	};
+
+	render() {
 		return (
-			<form className="user-form">
-				<input id="username" type="text" placeholder="username"/>
-				<input id="password" type="text" placeholder="password"/>
-				<div>
-					<label htmlFor="new-user-flag">New User?</label>
-					<input id="new-user-flag" type="checkbox" />
-				</div>
-				<div>
+			<div
+				id="modal-root"
+				// className="modal-layer"
+				className="modal-layer closed"
+				// onClick={this.handleFormClose}
+			>
+				<form className="user-form">
+					<input
+						id="username"
+						type="text"
+						placeholder="username"
+						name="uuid"
+						onChange={this.handleChange}
+					/>
+					<input
+						id="password"
+						type="text"
+						placeholder="password"
+						name="password"
+						onChange={this.handleChange}
+					/>
+					<div>
+						<label htmlFor="new-user-flag">New User?</label>
+						<input
+							id="new-user-flag"
+							type="checkbox"
+							name="newUser"
+							onChange={this.handleAuthType}
+						/>
+					</div>
+					<div>
+						<button
+							id="submit-form"
+							className="submit-btn ws-btn ws-blue"
+							onClick={this.handleFormSubmit}
+						>
+							{this.state.newUser ? "Create" : "Login"}
+						</button>
+					</div>
+				</form>
 
-					<button 
-						id="submit-form"
-						className="submit-btn"
-						onClick={this.handleFormSubmit}
-					>
-						Submit
-					</button>
-					{/* <button 
-						id="submit-login"
-						className="submit-btn"
-						onClick={this.handleLoginSubmit}
-					>
-						Login
-					</button> */}
-				</div>
-			</form>
-		)
+				<button
+					id="close-form"
+					className="close-btn ws-btn"
+					onClick={this.handleFormClose}
+				>
+					Close
+				</button>
+
+				{this.props.children}
+			</div>
+		);
 	}
 }
 export default UserForm;
